1e2a3829fcd1f7a97b7070ce8ceccd4f5ef91f29
Cloud_Garenasg <Cloud_Garenasg@bancodeoccidente.com.co>
2025-07-30 09:13:31 -0500

    actualizacion output

--- DynamoDB/outputs.tf
+++ DynamoDB/outputs.tf
@@ -1,16 +1,16 @@
-output "dynamodb_write_throttle_events" {
-  value = [for key, _ in var.dynamo_tables : aws_cloudwatch_metric_alarm.dynamodb_write_throttle_events[key].alarm_name]
-}
+# output "dynamodb_write_throttle_events" {
+#   value = [for key, _ in var.dynamo_tables : aws_cloudwatch_metric_alarm.dynamodb_write_throttle_events[key].alarm_name]
+# }
 
-output "dynamodb_system_errors" {
-  value = [for key, _ in var.dynamo_tables : aws_cloudwatch_metric_alarm.dynamodb_system_errors[key].alarm_name]
-}
+# output "dynamodb_system_errors" {
+#   value = [for key, _ in var.dynamo_tables : aws_cloudwatch_metric_alarm.dynamodb_system_errors[key].alarm_name]
+# }
 
-output "dynamodb_returned_bytes" {
-  value = [for key, _ in var.dynamo_tables : aws_cloudwatch_metric_alarm.dynamodb_returned_bytes[keys].alarm_name]
-}
+# output "dynamodb_returned_bytes" {
+#   value = [for key, _ in var.dynamo_tables : aws_cloudwatch_metric_alarm.dynamodb_returned_bytes[keys].alarm_name]
+# }
 
-output "dynamodb_success_fulrequest_latency" {
-  value = [for key, _ in var.dynamo_tables : aws_cloudwatch_metric_alarm.dynamodb_success_fulrequest_latency[key].alarm_name]
-}
+# output "dynamodb_success_fulrequest_latency" {
+#   value = [for key, _ in var.dynamo_tables : aws_cloudwatch_metric_alarm.dynamodb_success_fulrequest_latency[key].alarm_name]
+# }
 
